c33964d15da1dc3a45f4355ed72a64ccbd1d32c3
nikitarwida <rwidanikita@gmail.com>
2025-01-30 13:04:47 +0200

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayRecipe(response){
-    console.log("recipe generated");
     new Typewriter("#recipe", {
       strings: response.data.answer,
       autoStart: true,
@@ -10,16 +9,15 @@ function displayRecipe(response){
 function generateRecipe(event) {
   event.preventDefault();
 
- let instructionsInput=document.querySelector("#user-instructions");
+ let instructionsInput= document.querySelector("#user-instructions");
 let apiKey = "3a1ba03353eo46c2131e8effa9ted923";
-let context="You are a health and nutrition expert and love quick healthy recipes under 500 calories"
-let prompt=`User instructions: Generate quick healthy meal recipe about ${instructionsInput.value} must be quick and straight to the point and follow
-user instructions. Sign off with "SheCodes AI" inside a <strong> element.`
+let context="You are a health and nutrition expert and love quick healthy recipes under 500 calories.Your mission is to generate quicky healthy recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
+let prompt=`User instructions: Generate quick healthy meal recipe about ${instructionsInput.value};`
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("Generating recipe");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
+let recipeElement=document.querySelector("#recipe");
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML=`<div class="generating">⏳ Generating a ${instructionsInput.value} Healthy Recipe </div>`;
 
 axios.get(apiUrl).then(displayRecipe);
 }
